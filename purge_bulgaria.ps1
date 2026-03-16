2ae2419cd704b4a89e606e51b0d2f584d0a1336a
Lalyo Boyadzhiev <46906956+enemyssss@users.noreply.github.com>
2026-03-16 19:18:15 +0100

    Delete purge_bulgaria.ps1

--- purge_bulgaria.ps1
+++
@@ -1,6 +0,0 @@
-$f = "c:\Users\Lalyo Boyadzhiev\AppData\LocalLow\BlackSeaGames\Sovereign\Mods\681AD\Maps\europe\realms.csv"
-$c = [System.IO.File]::ReadAllText($f)
-# Replace Bulgaria (but not Bulgaria_681) with -
-# \bBulgaria\b(?!_681) matches 'Bulgaria' as a word, but only if not followed by '_681'
-$newC = $c -replace '\bBulgaria\b(?!_681)', '-'
-[System.IO.File]::WriteAllText($f, $newC)
